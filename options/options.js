4186015a489363139d881ed0dfef404a946972d6
kelvin4664 <kelvinadigwu@yahoo.com>
2023-09-12 12:47:38 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "why-nott-39",
+  url: "https://why-nott-39.botics.co",
+  api: "https://why-nott-39.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
